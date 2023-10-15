02a0bf5dd8578aacda4db14022b618ec0cb655af
Nirmay4512 <147958154+Nirmay4512@users.noreply.github.com>
2023-10-15 11:27:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('hello world')
+document.getElementById('add').onclick = function(){
+    let n1 = document.getElementById('first number').value;
+    n1 = Number(n1);
+    let n2 = document.getElementById('second number').value;
+    n2 = Number(n2);
+        document.getElementById('mylabel').innerHTML = n1+=n2;
+}
+document.getElementById('sub').onclick = function(){
+    let n1 = document.getElementById('first number').value;
+    n1 = Number(n1);
+    let n2 = document.getElementById('second number').value;
+    n2 = Number(n2);
+    document.getElementById('mylabel').innerHTML = n1-=n2;
+}
+document.getElementById('divide').onclick = function(){
+    let n1 = document.getElementById('first number').value;
+    n1 =Number(n1);
+    let n2 = document.getElementById('second number').value;
+    n2 = Number(n2);
+    document.getElementById('mylabel').innerHTML = n1/n2;
+}
+document.getElementById('multiply').onclick = function(){
+    let n1 = document.getElementById('first number').value;
+    n1 = Number(n1);
+    let n2 = document.getElementById('second number').value;
+    n2 = Number(n2);
+    document.getElementById('mylabel').innerHTML = n1*n2;
+}
+document.getElementById('clear').onclick = function(){
+    window.location.reload();
+}
+    
+    
+    
+    
